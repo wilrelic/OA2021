bbeb24335ba4468efdb6576bc62341493008b72d
wilrelic <74131785+wilrelic@users.noreply.github.com>
2021-06-09 14:02:07 +0800

    fixed footer on location page

--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="container">
-    <h1>Our Locations</h1>
-      <div class="location">
-        <div class="rockingham">
-          <h4>ROCKINGHAM WA</h4>
-            <a href="https://www.google.com/maps/place/45+Dixon+Rd,+Rockingham+WA+6168/@-32.2761326,115.7512398,17z/data=!3m1!4b1!4m5!3m4!1s0x2a328354c1519b63:0x391085ef6ec658a0!8m2!3d-32.2761372!4d115.7534285" target="_blank">
-              <img src="../assets/imgs/rocky.png" />
-            </a>
-          <p>
-            45 Dixon Road
-            Rockingham WA
-            6168
-          </p>
-          <p>
-            Tel: 
-            <a href="tel:+61895284300">(08) 9528 4300</a>
-          </p>
-        </div>
-        <div class="mandurah">
-          <h4>MANDURAH WA</h4>
-            <a href="https://www.google.com/maps/place/22+Panton+Rd,+Greenfields+WA+6210/@-32.5141139,115.7440563,17z/data=!3m1!4b1!4m5!3m4!1s0x2a327ea35062db29:0x5310488fc8b88215!8m2!3d-32.5141184!4d115.746245" target="_blank">
-              <img src="../assets/imgs/mand.png" />
-            </a>
-          <p>
-            22 Panton Road
-            Mandurah WA
-            6120
-          </p>
-          <p>
-            Tel: 
-            <a href="tel:+61895205255">(08) 9520 5255</a>
-          </p>
+      <div class="location-wrapper">
+        <h1>Our Locations</h1>
+        <div class="location">
+          <div class="rockingham">
+            <h4>ROCKINGHAM WA</h4>
+              <a href="https://www.google.com/maps/place/45+Dixon+Rd,+Rockingham+WA+6168/@-32.2761326,115.7512398,17z/data=!3m1!4b1!4m5!3m4!1s0x2a328354c1519b63:0x391085ef6ec658a0!8m2!3d-32.2761372!4d115.7534285" target="_blank">
+                <img src="../assets/imgs/rocky.png" />
+              </a>
+            <p>
+              45 Dixon Road
+              Rockingham WA
+              6168
+            </p>
+            <p>
+              Tel: 
+              <a href="tel:+61895284300">(08) 9528 4300</a>
+            </p>
+          </div>
+          <div class="mandurah">
+            <h4>MANDURAH WA</h4>
+              <a href="https://www.google.com/maps/place/22+Panton+Rd,+Greenfields+WA+6210/@-32.5141139,115.7440563,17z/data=!3m1!4b1!4m5!3m4!1s0x2a327ea35062db29:0x5310488fc8b88215!8m2!3d-32.5141184!4d115.746245" target="_blank">
+                <img src="../assets/imgs/mand.png" />
+              </a>
+            <p>
+              22 Panton Road
+              Mandurah WA
+              6120
+            </p>
+            <p>
+              Tel: 
+              <a href="tel:+61895205255">(08) 9520 5255</a>
+            </p>
+          </div>
         </div>
       </div>
       <CallUs />
@@ -56,7 +58,7 @@ export default {
     text-align: center;
   }
 
-  .container {
+  .location-wrapper {
     margin-top: 100px;
   }
 
